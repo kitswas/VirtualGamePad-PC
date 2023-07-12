ef237ca9a2af04eb08767326b5e4606099645d43
kitswas <90329875+kitswas@users.noreply.github.com>
2023-07-13 00:58:39 +0530

    Project Setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,11 @@
-/**
- * @file main.cpp
- * @brief Just trying out doxygen
- * @author Does it matter?
- * @version 0.1
- * @date 2022-11-02
- *
- * @copyright Copyright (c) 2022
- *
- * Comments _must_ be written in [**Markdown**](https://www.markdownguide.org/cheat-sheet/).
- */
+#include "mainwindow.hpp"
 
-#include <iostream>
+#include <QApplication>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char *argv[])
+{
+	QApplication a(argc, argv);
+	MainWindow w;
+	w.show();
+	return a.exec();
 }
