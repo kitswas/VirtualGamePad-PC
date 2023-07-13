400350bfe492ceb59e373f9198dd850745d64f83
kitswas <90329875+kitswas@users.noreply.github.com>
2023-07-13 18:42:55 +0530

    Include guard change

--- src/mainwindow.hpp
+++ src/mainwindow.hpp
@@ -1,5 +1,4 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#pragma once
 
 #include <QMainWindow>
 
@@ -21,4 +20,3 @@ class MainWindow : public QMainWindow
   private:
 	Ui::MainWindow *ui;
 };
-#endif // MAINWINDOW_H
