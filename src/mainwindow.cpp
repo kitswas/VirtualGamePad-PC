2f78e29233bdaf11f49a5887f17e260cb8718d05
kitswas <90329875+kitswas@users.noreply.github.com>
2023-07-14 21:33:27 +0530

    Better status reporting

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,7 +1,7 @@
 #include "mainwindow.hpp"
 #include "networking/server.hpp"
 
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
--- src/networking/server.cpp
+++ src/networking/server.cpp
@@ -32,13 +32,13 @@ void Server::initServer()
 		close();
 		return;
 	}
-	QString message =
-		tr("The server is running on\n\nPort: %1\n\nAt the following IP Address(es):\n\n").arg(tcpServer->serverPort());
+	QString message = tr("The server is running on\n\nPort: `%1`\n\nAt the following IP Address(es):\n\n")
+						  .arg(tcpServer->serverPort());
 	const QList<QHostAddress> ipAddressesList = QNetworkInterface::allAddresses();
 	for (const QHostAddress &entry : ipAddressesList)
 	{
 		if (entry.isGlobal())
-			message += tr("%1\n").arg(entry.toString());
+			message += tr("`%1`\n").arg(entry.toString());
 	}
 	ui->statusLabel->setText(message);
 	connect(tcpServer, &QTcpServer::newConnection, this, &Server::handleConnection);
@@ -47,9 +47,20 @@ void Server::initServer()
 void Server::handleConnection()
 {
 	QTcpSocket *clientConnection = tcpServer->nextPendingConnection();
-	qDebug() << "Established connection with" << clientConnection->peerName() << "at"
-			 << clientConnection->peerAddress().toString() << ":" << clientConnection->peerPort();
+	QString connectionMessage;
+	connectionMessage =
+		tr("Connected to %1 at `%2 : %3`")
+			.arg(clientConnection->peerName().isEmpty() ? "Unknown device" : clientConnection->peerName(),
+				 clientConnection->peerAddress().toString(), QString::number(clientConnection->peerPort()));
+	qDebug() << connectionMessage;
+	ui->clientLabel->setText(connectionMessage);
 	connect(clientConnection, &QAbstractSocket::disconnected, clientConnection, &QObject::deleteLater);
+	connect(clientConnection, &QAbstractSocket::disconnected, this, [this]() {
+		// Perform Null checks, because this lambda is also called when the server is closed
+		// Note, the order is important
+		if (this != nullptr && ui != nullptr && ui->clientLabel != nullptr)
+			ui->clientLabel->setText(tr("No client connected"));
+	});
 	connect(clientConnection, &QAbstractSocket::readyRead, this, [clientConnection]() {
 		qDebug() << "Received: " << clientConnection->bytesAvailable() << "bytes";
 		QByteArray request = clientConnection->readAll();
