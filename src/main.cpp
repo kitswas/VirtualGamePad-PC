c8fc847e24122b7f0a1391767da2e5447895c061
kitswas <90329875+kitswas@users.noreply.github.com>
2023-07-15 15:19:55 +0530

    Set 'Fusion' style

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <QApplication>
+#include <QStyleFactory>
 
 #include "mainwindow.hpp"
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	a.setStyle(QStyleFactory::create("Fusion"));
 	MainWindow w;
 	w.show();
 	return a.exec();
