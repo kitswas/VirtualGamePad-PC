0835e342732ffcc9ebf627953618efbb38d9d671
kitswas <90329875+kitswas@users.noreply.github.com>
2024-03-31 20:56:11 +0530

    Add application metadata

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@ int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	a.setStyle(QStyleFactory::create("Fusion"));
+	a.setOrganizationName("Kitswas");
+	a.setOrganizationDomain("io.github.kitswas");
+	a.setApplicationName("VirtualGamePad");
 	MainWindow w;
 	w.show();
 	return a.exec();
