b2026f51a4ec99188916f027e0e1a0f8d7ecf76f
kitswas <90329875+kitswas@users.noreply.github.com>
2025-05-23 13:43:54 +0530

    Badges on About page

---
+++ src/ui/badge.cpp
@@ -0,0 +1,62 @@
+#include "badge.hpp"
+
+#include <QDesktopServices>
+#include <QMouseEvent>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QPixmap>
+
+Badge::Badge(QWidget *parent) : QLabel(parent), networkManager(new QNetworkAccessManager(this))
+{
+	setCursor(Qt::PointingHandCursor);
+}
+
+Badge::~Badge()
+{
+	// QNetworkAccessManager deleted as a child
+}
+
+void Badge::loadBadge(const QString &imageUrl, const QString &linkUrl)
+{
+	this->linkUrl = linkUrl;
+
+	// Fix the "most vexing parse" issue by using braces initialization
+	QNetworkRequest request{QUrl(imageUrl)};
+	QNetworkReply *reply = networkManager->get(request);
+
+	connect(reply, &QNetworkReply::finished, this, &Badge::onNetworkReplyFinished);
+}
+
+void Badge::mousePressEvent(QMouseEvent *event)
+{
+	if (event->button() == Qt::LeftButton && !linkUrl.isEmpty())
+	{
+		QDesktopServices::openUrl(QUrl(linkUrl));
+		event->accept();
+	}
+	else
+	{
+		QLabel::mousePressEvent(event);
+	}
+}
+
+void Badge::onNetworkReplyFinished()
+{
+	QNetworkReply *reply = qobject_cast<QNetworkReply *>(sender());
+	if (!reply)
+		return;
+
+	if (reply->error() == QNetworkReply::NoError)
+	{
+		QPixmap pixmap;
+		pixmap.loadFromData(reply->readAll());
+		setPixmap(pixmap);
+
+		if (!linkUrl.isEmpty())
+		{
+			setToolTip(linkUrl);
+		}
+	}
+
+	reply->deleteLater();
+}
