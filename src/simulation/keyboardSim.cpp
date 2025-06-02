5888755a3863fecb6142be209eabfc5751711868
kitswas <90329875+kitswas@users.noreply.github.com>
2025-06-03 00:17:06 +0530

    More accurate keyboard simulation
    Pass in Scan codes for some internationalized games like Assassin's Creed and Dave the Diver.

--- src/simulation/keyboardSim.cpp
+++ src/simulation/keyboardSim.cpp
@@ -1,15 +1,30 @@
 #include "keyboardSim.hpp"
 
+#include <unordered_set>
+
+// Extended key lookup table.
+// Reference:
+// https://learn.microsoft.com/en-us/windows/win32/inputdev/about-keyboard-input#extended-key-flag
+static const std::unordered_set<WORD> extendedKeys = {
+	VK_RMENU, VK_RCONTROL, VK_INSERT, VK_DELETE, VK_HOME,	 VK_END,	VK_PRIOR,	 VK_NEXT,
+	VK_RIGHT, VK_UP,	   VK_LEFT,	  VK_DOWN,	 VK_NUMLOCK, VK_CANCEL, VK_SNAPSHOT, VK_DIVIDE};
+
 void pressKey(WORD key)
 {
 	INPUT input = {0};
 	input.type = INPUT_KEYBOARD;
 	input.ki.wVk = key;
+	input.ki.wScan = static_cast<WORD>(MapVirtualKeyW(key, MAPVK_VK_TO_VSC));
+	input.ki.dwFlags = KEYEVENTF_SCANCODE;
+	if (extendedKeys.contains(key))
+	{
+		input.ki.dwFlags |= KEYEVENTF_EXTENDEDKEY;
+	}
 	SendInput(1, &input, sizeof(INPUT));
 	// Wait
 	Sleep(PRESS_INTERVAL);
 	// Release the key
-	input.ki.dwFlags = KEYEVENTF_KEYUP;
+	input.ki.dwFlags |= KEYEVENTF_KEYUP;
 	SendInput(1, &input, sizeof(INPUT));
 }
 
@@ -20,6 +35,12 @@ void pressKeyCombo(std::vector<WORD> keys)
 	{
 		inputs[i].type = INPUT_KEYBOARD;
 		inputs[i].ki.wVk = keys[i];
+		inputs[i].ki.wScan = static_cast<WORD>(MapVirtualKeyW(keys[i], MAPVK_VK_TO_VSC));
+		inputs[i].ki.dwFlags = KEYEVENTF_SCANCODE;
+		if (extendedKeys.contains(keys[i]))
+		{
+			inputs[i].ki.dwFlags |= KEYEVENTF_EXTENDEDKEY;
+		}
 	}
 	// Wait
 	Sleep(PRESS_INTERVAL);
@@ -27,18 +48,14 @@ void pressKeyCombo(std::vector<WORD> keys)
 	{
 		inputs[i + keys.size()].type = INPUT_KEYBOARD;
 		inputs[i + keys.size()].ki.wVk = keys[i];
-		inputs[i + keys.size()].ki.dwFlags = KEYEVENTF_KEYUP;
+		inputs[i + keys.size()].ki.wScan = static_cast<WORD>(MapVirtualKeyW(keys[i], MAPVK_VK_TO_VSC));
+		inputs[i + keys.size()].ki.dwFlags = KEYEVENTF_KEYUP | KEYEVENTF_SCANCODE;
+		if (extendedKeys.contains(keys[i]))
+		{
+			inputs[i + keys.size()].ki.dwFlags |= KEYEVENTF_EXTENDEDKEY;
+		}
 	}
 	SendInput(keys.size() * 2, inputs.data(), sizeof(INPUT));
-}
-
-void keyUp(WORD key)
-{
-	INPUT input = {0};
-	input.type = INPUT_KEYBOARD;
-	input.ki.wVk = key;
-	input.ki.dwFlags = KEYEVENTF_KEYUP;
-	SendInput(1, &input, sizeof(INPUT));
 }
 
 void keyDown(WORD key)
@@ -46,6 +63,26 @@ void keyDown(WORD key)
 	INPUT input = {0};
 	input.type = INPUT_KEYBOARD;
 	input.ki.wVk = key;
+	input.ki.wScan = static_cast<WORD>(MapVirtualKeyW(key, MAPVK_VK_TO_VSC));
+	input.ki.dwFlags = KEYEVENTF_SCANCODE;
+	if (extendedKeys.contains(key))
+	{
+		input.ki.dwFlags |= KEYEVENTF_EXTENDEDKEY;
+	}
+	SendInput(1, &input, sizeof(INPUT));
+}
+
+void keyUp(WORD key)
+{
+	INPUT input = {0};
+	input.type = INPUT_KEYBOARD;
+	input.ki.wVk = key;
+	input.ki.wScan = static_cast<WORD>(MapVirtualKeyW(key, MAPVK_VK_TO_VSC));
+	input.ki.dwFlags = KEYEVENTF_KEYUP | KEYEVENTF_SCANCODE;
+	if (extendedKeys.contains(key))
+	{
+		input.ki.dwFlags |= KEYEVENTF_EXTENDEDKEY;
+	}
 	SendInput(1, &input, sizeof(INPUT));
 }
 
@@ -56,7 +93,12 @@ void keyComboUp(std::vector<WORD> keys)
 	{
 		inputs[i].type = INPUT_KEYBOARD;
 		inputs[i].ki.wVk = keys[i];
-		inputs[i].ki.dwFlags = KEYEVENTF_KEYUP;
+		inputs[i].ki.wScan = static_cast<WORD>(MapVirtualKeyW(keys[i], MAPVK_VK_TO_VSC));
+		inputs[i].ki.dwFlags = KEYEVENTF_KEYUP | KEYEVENTF_SCANCODE;
+		if (extendedKeys.contains(keys[i]))
+		{
+			inputs[i].ki.dwFlags |= KEYEVENTF_EXTENDEDKEY;
+		}
 	}
 	SendInput(keys.size(), inputs.data(), sizeof(INPUT));
 }
@@ -68,6 +110,12 @@ void keyComboDown(std::vector<WORD> keys)
 	{
 		inputs[i].type = INPUT_KEYBOARD;
 		inputs[i].ki.wVk = keys[i];
+		inputs[i].ki.wScan = static_cast<WORD>(MapVirtualKeyW(keys[i], MAPVK_VK_TO_VSC));
+		inputs[i].ki.dwFlags = KEYEVENTF_SCANCODE;
+		if (extendedKeys.contains(keys[i]))
+		{
+			inputs[i].ki.dwFlags |= KEYEVENTF_EXTENDEDKEY;
+		}
 	}
 	SendInput(keys.size(), inputs.data(), sizeof(INPUT));
 }
