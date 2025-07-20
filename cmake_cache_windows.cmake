77320348c2cb19714d588e1f0ce39f21b64a4658
kitswas <90329875+kitswas@users.noreply.github.com>
2025-07-20 20:21:06 +0530

    MinGW config in CMake cache
    To prevent using the system MinGW, if any.

--- cmake_cache_windows.cmake
+++ cmake_cache_windows.cmake
@@ -7,3 +7,8 @@ set(QT_VERSION "6.8.3")
 set(QT_COMPILER "mingw_64") # or "msvc2022_64" based on your compiler
 set(CMAKE_PREFIX_PATH "D:/Qt/${QT_VERSION}/${QT_COMPILER}" CACHE STRING "Qt installation path")
 message(STATUS "Using Qt6 from: ${CMAKE_PREFIX_PATH}")
+
+# MinGW configuration (to ensure the correct compiler is used)
+set(QT_BUNDLED_MINGW "D:/Qt/Tools/mingw1310_64")
+set(CMAKE_C_COMPILER "${QT_BUNDLED_MINGW}/bin/gcc.exe" CACHE FILEPATH "C compiler")
+set(CMAKE_CXX_COMPILER "${QT_BUNDLED_MINGW}/bin/g++.exe" CACHE FILEPATH "C++ compiler")
