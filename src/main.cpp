2ee815bd95df97c1eddd114ce720dd1167861bdc
kitswas <90329875+kitswas@users.noreply.github.com>
2025-12-28 10:06:37 +0530

    Multi-instance logging

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,14 @@
 #include "ui/mainwindow.hpp"
 
 #include <QApplication>
+#include <QDir>
 #include <QFile>
+#include <QFileInfo>
+#include <QLockFile>
 #include <QMutex>
 #include <QStandardPaths>
 #include <QStyleFactory>
+#include <memory>
 
 #if defined(QT_DEBUG)
 const QString logFilePath = "virtualgamepad.log";
@@ -16,6 +20,12 @@ const QString logFilePath = "virtualgamepad.log";
 const QString logFilePath =
 	QStandardPaths::writableLocation(QStandardPaths::TempLocation) + "/virtualgamepad.log";
 #endif
+
+/**
+ * @brief Maximum number of log files to try to create.
+ * If all are in use, logging to file will be disabled.
+ */
+const int8_t max_log_files = 5;
 
 static QFile logFile(logFilePath);
 
@@ -39,10 +49,42 @@ int main(int argc, char *argv[])
 	attachToParentConsole();
 #endif
 
-	qSetMessagePattern("[%{time yyyy-MM-ddTHH:mm:ss.zzz} %{type}] %{message}");
-	bool logFileOpened =
-		logFile.open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Unbuffered |
-					 QIODevice::Text | QIODevice::Truncate);
+	// Try to find a free log file slot (virtualgamepad.1.log, virtualgamepad.2.log, etc.)
+	static std::unique_ptr<QLockFile> lockFile;
+	QFileInfo baseInfo(logFilePath);
+	QString baseName = baseInfo.completeBaseName();
+	QString suffix = baseInfo.suffix();
+	// Use the directory from the configured path, or current dir if none specified
+	QDir logDir = baseInfo.dir();
+
+	for (int i = 1; i <= max_log_files; ++i)
+	{
+		QString fileName = QString("%1.%2.%3").arg(baseName).arg(i).arg(suffix);
+		QString fullPath = logDir.filePath(fileName);
+
+		auto l = std::make_unique<QLockFile>(fullPath + ".lock");
+		l->setStaleLockTime(0);
+		if (l->tryLock())
+		{
+			lockFile = std::move(l);
+			logFile.setFileName(fullPath);
+			break;
+		}
+	}
+
+	qSetMessagePattern("[%{time yyyy-MM-ddTHH:mm:ss.zzz} %{pid} %{type}] %{message}");
+
+	bool logFileOpened = false;
+	if (lockFile)
+	{
+		logFileOpened = logFile.open(QIODevice::WriteOnly | QIODevice::Truncate |
+									 QIODevice::Unbuffered | QIODevice::Text);
+	}
+	else
+	{
+		qWarning() << "Failed to acquire lock for any log file slot (1-" << max_log_files << "). "
+		"Logging to file disabled.";
+	}
 
 	qInfo() << "Log file path:" << QFileInfo(logFile).absoluteFilePath();
 	QtMessageHandler oldMessageHandler;
